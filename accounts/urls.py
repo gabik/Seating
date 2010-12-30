0acb7e0e961976bce5785d0ad40957ef0b513212
Gabi Kazav <gabi.com@gmail.com>
2010-12-30 07:09:49 -0700

    Login and Logout Admin site By gabik

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.views import login, logout, password_reset_confirm
 from django.conf.urls.defaults import *
+from Seating.accounts import views
 
 urlpatterns = patterns('accounts.views',
 	(r'^login/$', login),
 	# (r'^new/$', 'create_user'),
 	(r'^logout/$', logout),
+	(r'^is_login/$', 'is_login'),
 )
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+
+
+#@login_required
+def is_login(request):
+	if request.user.is_authenticated():
+		return HttpResponse('Hi ' +  request.user.username + ': ' + request.user.get_full_name() + '<BR> <form action=/users/logout> <input type=submit value=Logout> </form>')
+	else: # Nothing has been posted
+		return HttpResponse('Please login')
