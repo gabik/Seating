78717ed78e037be8d8aa7fb0f2126b41200a121c
root <root@ip-10-194-6-34.ec2.internal>
2011-09-26 02:35:43 -0400

    Site addded

---
+++ apache/django.wsgi
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.append('/Seating')
+sys.path.append('/')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Seating.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
---
+++ static/locale/he/he.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def u(he_str):
+	return unicode(he_str, "UTF-8")
+
+first_name=u("שם פרטי")
+last_name=u("שם משפחה")
+quantity=u("כמות")
+phone_number=u("מספר טלפון")
+email=u("דואר אלקטרוני")
+facebook=u("פייסבוק")
+group=u("קבוצה")
+present=u("סכום מתנה")
