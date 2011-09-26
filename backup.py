0118300e249d77d5d8a2833cea5ca6d33200fc18
root <root@ip-10-194-6-34.ec2.internal>
2011-09-26 06:53:50 -0400

    backup start script

---
+++ backup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+import sys
+import os
+import time
+import tarfile
+
+def tarToS3(directoryName='/Seating', s3Bucket='2seat', s3Key=None, s3AcctId='AKIAIFUNDQAYFXW6RURA', s3SecretAccess='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' ):
+        contents = os.listdir(directoryName)
+	curtime=str(time.strftime('%d%m%H%M%S'))
+        if s3Key==None:
+                s3Key="backup."+curtime+".tar.gx"
+        else:
+                s3Key+='-'+curtime+'.tar.gz'
+
+        backupFile = "/backups/"+s3Key
+	os.system("mysqldump --all-databases > /backups/dbs/"+curtime+".sqldump")
+        tar = tarfile.open(backupFile, "w:gz")
+	tar.add("/backups/dbs/"+curtime+".sqldump")	
+        for item in contents:
+                tar.add(directoryName+'/'+item)
+        tar.close()
+
+        if s3SecretAccess==None:
+                conn = S3Connection()
+        else:
+                conn = S3Connection(s3AcctId, s3SecretAccess)
+
+        bucket = conn.get_bucket(s3Bucket)
+        k = Key(bucket)
+        k.key = s3Key
+	k.set_contents_from_filename(backupFile)
+
+tarToS3(s3Key='Regular')
