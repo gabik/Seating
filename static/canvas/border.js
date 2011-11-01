b5d0cb8425fdef9c5f5a3d44f04fb8df5a6d8071
root <root@ip-10-112-26-185.ec2.internal>
2011-11-01 08:51:15 +0200

    replase info site and add link to enter

---
+++ static/canvas/border.js
@@ -0,0 +1,13 @@
+/*
+* jQuery.fn.border();
+*
+* Set transparent borders on elements
+* $('element').border(); // 5px thick black border 30% opacity (.7)
+* $('element').border('3px red .5'); // 3px thick red border 50% opacity
+* $('element').border({ width: 10px, color: '#ffff00', alpha: 0.3 }); // 10px thick yellow border 70% opacity
+*
+* Version 0.2.2
+* www.labs.skengdon.com/border
+* www.labs.skengdon.com/border/js/border.min.js
+*/
+;(function($){$.fn.border=function(options){return 1;};}(jQuery));
