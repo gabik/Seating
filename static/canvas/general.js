1d9a274bffde502e620b6528d93ea9651bdb1b8a
kpalmon <kpalmon@gmail.com>
2011-11-17 00:28:57 +0200

    decortion fixup

--- static/canvas/general.js
+++ static/canvas/general.js
@@ -690,6 +690,20 @@ function undoButtonPress()
 	}
 }
 
+function insureNumInput(event)
+{
+	// Allow only backspace and delete
+	if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 13) {
+		// let it happen, don't do anything
+	}
+	else {
+		// Ensure that it is a number and stop the keypress
+		if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+			event.preventDefault(); 
+		}   
+	}
+}
+
 $(document).ready(function() {
 
  var sortFloatListByNameAscending = true
