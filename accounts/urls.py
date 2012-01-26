8b83eba79ecd500410e09969d32210ce7cafea05
koren palmon - Server <koren@2seat-big.localdomain>
2012-01-26 01:58:17 -0500

    changes in meting

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,6 +17,5 @@ urlpatterns = patterns('accounts.views',
 	(r'^map/$', 'download_map'),
 	(r'^registered/$', 'registered'),
 	(r'^do_duplicates/$', 'do_duplicates'),
-	(r'^invation/(?P<first>\w+)-(?P<last>\w+)/$', 'invation'),
-	#(r'^changePersonInvation/$', 'change_person_invation'),
+	(r'^invation/(?P<guestHash>\w+)/$', 'invation'),
 )
